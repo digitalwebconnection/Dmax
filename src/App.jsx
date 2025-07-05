c0a7e1ffab79207ad56f345ef8fd211c00b0bdf2
digitalwebconnection <service@digitalwebconnection.com>
2025-07-05 13:11:30 +0530

    ad commwwww

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
 
       <Routes>
         <Route path="/"         element={<Home />} />
-        <Route path="/about" element={<AboutUs />} />
+        <Route path="/about"    element={<div>About Us</div>} />
       </Routes>
 
       <Footer />
